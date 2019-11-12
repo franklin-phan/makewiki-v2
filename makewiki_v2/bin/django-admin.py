a1e20d514578dfd7b20f64a6c08b66e538cdccb8
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-11-12 14:49:50 -0800

    first commit

---
+++ makewiki_v2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/franklinphan/dev/courses/bew12/makewikiv2/makewiki_v2/makewiki_v2/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from wiki.views import PageListView, PageDetailView
+from django.conf.urls import include
 
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
